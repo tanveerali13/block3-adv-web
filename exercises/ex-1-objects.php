9554e2dc58a1b775687d2d0964ea541b1ac7f982
tanveerali13 <tanveer.nsu13@gmail.com>
2023-11-04 15:56:48 -0400

    Revised Version

--- exercises/ex-1-objects.php
+++ exercises/ex-1-objects.php
@@ -4,98 +4,121 @@ ini_set("display_errors", 1);
 class Car
 {
     private $name;
-    private $year;
-    private $isRunning;
+    private $model;
+    private $turnKeyOn;
+    private $engineOn;
+    private $pedalPressed;
+    private $isMoving;
     private $fuelLevel;
 
-    public function __construct($name, $year)
+    public function __construct($name, $model)
     {
         $this->name = $name;
-        $this->year = $year;
-        $this->isRunning = false;
+        $this->model = $model;
+        $this->turnKeyOn = false;
+        $this->engineOn = false;
+        $this->pedalPressed = false;
+        $this->isMoving = false;
         $this->fuelLevel = 100;
     }
 
-    public function status()
+    public function showStatus()
     {
-        echo "Car name = $this->name <br>
-              Make year = $this->year <br>
-              Fuel level = $this->fuelLevel <br>
-              The car is not running <br>";
-
+        echo "----------STATUS --------- <br>";
+        echo "Car Name: $this->name <br>";
+        echo "Car Model: $this->model <br>";
+        echo "Key is turned " . ($this->turnKeyOn ? "on" : "off") . "<br>";
+        echo "Engine is " . ($this->engineOn ? "running" : "off") . "<br>";
+        echo "Fuel Level: $this->fuelLevel% <br>";
+        echo "------------------------- <br>";
     }
 
-    public function start()
+    public function turnOnKey()
     {
-        if (!$this->isRunning) {
-            $this->turnOnKey();
-            $this->isRunning = true;
+        if (!$this->turnKeyOn) {
+            $this->turnKeyOn = true;
+            echo "Key turned on <br>";
+            $this->startEngine();
+        } else {
+            echo "Engine is already running. <br>";
+        }
+    }
+
+    public function turnOffKey()
+    {
+        if ($this->turnKeyOn) {
+            $this->turnKeyOn = false;
+            echo "Key turned off. <br>";
+            $this->stopEngine();
+        } else {
+            echo "key is already turned off <br>";
+        }
+    }
+
+    public function startEngine()
+    {
+        if ($this->turnKeyOn) {
+            $this->engineOn = true;
+            echo "Engine started <br>";
+            $this->fuelLevel = 90;
+        } else {
+            echo "Turn the key first to start the engine <br>";
+        }
+    }
+
+    private function stopEngine()
+    {
+        if (!$this->turnKeyOn) {
+            $this->engineOn = false;
+            echo "Engine stopped <br>";
+        }
+    }
+
+    public function moveForward()
+    {
+        if ($this->engineOn) {
+            $this->pedalPressed = true;
+            $this->isMoving = true;
+            echo "Car is moving forward <br>";
             $this->fuelLevel = 80;
-            echo "Car is running, fuel level = $this->fuelLevel <br>";
         } else {
-            echo "Car is already running. <br>";
+            echo "Start the engine first <br>";
         }
     }
 
-    private function turnOnKey()
+    public function applyBrakes()
     {
-        echo "Key turned ON <br>";
-        $this->accelerate();
-    }
-
-    private function accelerate()
-    {
-        echo "Accelerator pressed! <br>";
-    }
-
-    public function stop()
-    {
-        if ($this->isRunning) {
-            $this->brake();
-            $this->isRunning = false;
-            echo "The car stopped <br>";
-            $this->turnOffKey();
-        } else {
-            echo "Car is alredy on halt. Start the car first. <br>";
+        if ($this->isMoving) {
+            $this->isMoving = false;
+            echo "Brakes applied and car stopped moving. <br>";
         }
-    }
-
-    private function brake()
-    {
-        echo "Brakes applied! <br>";
-    }
-
-    private function turnOffKey()
-    {
-        echo "Key turned OFF <br>";
     }
 
     public function reFuel()
     {
         if ($this->fuelLevel < 100) {
             $this->fuelLevel = 100;
-            echo "Refuel Complete <br>";
+            echo "Refuel Complete. Fuel level is: $this->fuelLevel <br>";
         } else {
-            echo "Tank is already full <br>";
+            echo "Tank is full <br>";
         }
     }
 
 }
 
-$car1 = new Car('Mazda3', '2011');
-$car1->start();
-$car1->start();
-$car1->stop();
-$car1->stop();
-$car1->reFuel();
-$car1->reFuel();
+$car1 = new Car('Mazda3', '2020');
+
+$car1->showStatus();
+$car1->startEngine();
+$car1->turnOnKey();
+$car1->moveForward();
+$car1->refuel();
+$car1->applyBrakes();
+$car1->turnOffKey();
 
 
 
 
-$car2 = new Car('Toyota', '2020');
-$car2->start();
-$car2->refuel();
 
 
 
