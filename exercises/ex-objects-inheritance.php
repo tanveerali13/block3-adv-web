4dad6855d59ddbcc2c4da948a88f30b9b78bbcc8
tanveerali13 <tanveer.nsu13@gmail.com>
2023-11-26 23:35:38 -0500

    exercise

--- exercises/ex-objects-inheritance.php
+++ exercises/ex-objects-inheritance.php
@@ -1,20 +1,6 @@
-<!-- 
-    
 
-
-
-
-
-   
-
-    ### PROBLEM 8 ###
-    Write a php class called Shape with methods called getPerimeter() and getArea(). Create a subclass called Circle that overrides the getPerimeter() and getArea() methods to calculate the area and perimeter of a circle.
-
-    ### PROBLEM 9 ###
-    Write a php cehicle class hierarchy. The base class should be Vehicle, with subclasses Truck, Car and Motorcycle. Each subclass should have properties such as make, model, year, and fuel type. Implement methods for calculating fuel efficiency, distance traveled, and maximum speed.
-
-    ### PROBLEM 10 ###
-    Write a php ca class hierarchy for employees of a company. The base class should be Employee, with subclasses Manager, Developer, and Programmer. Each subclass should have properties such as name, address, salary, and job title. Implement methods for calculating bonuses, generating performance reports, and managing projects.-->
+<!-- ### PROBLEM 1 ###
+Write a php class called Animal with a method called makeSound(). Create a subclass called Cat that overrides the makeSound() method to bark.-->
 
 <?php
     ini_set('display_errors', 1);
@@ -61,6 +47,7 @@ $cat->makeSound();
 
 <!-- ### PROBLEM 3 ###
     Write a php class called Shape with a method called getArea(). Create a subclass called Rectangle that overrides the getArea() method to calculate the area of a rectangle.  -->
+    
 <?php 
     class Shape{
 
@@ -184,7 +171,7 @@ $cat->makeSound();
     }
     class Cheetah extends Animall{
         public function move(){
-            echo "Run Run <br>";
+            echo "Run <br>";
         }
     }
     
@@ -243,9 +230,70 @@ $cat->makeSound();
             return $this->lastName;
         }
     }
-    echo "end";
-    $employee1 = new Employees('Tanveer', 'Ali', '10', 'Web Developer');
-    echo $employee1->getJobTitle(). "Hello";
 
+    $employee1 = new Employees('Tanveer', 'Ali', '10', 'Web Developer');
+    echo $employee1->getJobTitle() . "</br>";
+    $employee2 = new Employees('Raisa', 'Khan', '11', 'Assistant Planner');
+    echo "Employee ID:" . $employee2->getEmployeeId() . "  Last Name:" . $employee2->getLastName() . "  Job Title:" . $employee2->getJobTitle() . "</br>" ;
+
+?>
+
+
+<!-- ### PROBLEM 8 ###
+    Write a php class called Shape with methods called getPerimeter() and getArea(). Create a subclass called Circle that overrides the getPerimeter() and getArea() methods to calculate the area and perimeter of a circle. -->
+<?php
+
+    class Shapes{
+        public function getPerimeter(){
+            echo "This is the Perimeter </br>";
+        }
+
+        public function getArea(){
+            echo "This is the Area </br>";
+        }
+    }
+    class Circles extends Shapes{
+        private $radius;
+
+        public function __construct($radius){
+            $this->radius = $radius;
+        }
+
+        public function getPerimeter(){
+            return 2 * pi() * $this->radius;
+        }
+
+        public function getArea(){
+            return 2 * pi() * ($this->radius)^2;
+        }
+    }
+
+    $circles1 = new Shapes();
+    $circles1->getPerimeter();
+    $circles1->getArea();
+
+    $circles2 = new Circles(5);
+    echo "Perimeter =" . $circles2->getPerimeter() . "</br>";
+    echo "Area =" . $circles2->getArea() . "</br>";
+?>
+
+
+<!-- ### PROBLEM 9 ###
+    Write a php vehicle class hierarchy. The base class should be Vehicle, with subclasses Truck, Car and Motorcycle. Each subclass should have properties such as make, model, year, and fuel type. Implement methods for calculating fuel efficiency, distance traveled, and maximum speed. -->
+
+<?php
+
+     class Vehicles{
+
+     }
+
+?>
+
+
+
+<!-- ### PROBLEM 10 ###
+    Write a php ca class hierarchy for employees of a company. The base class should be Employee, with subclasses Manager, Developer, and Programmer. Each subclass should have properties such as name, address, salary, and job title. Implement methods for calculating bonuses, generating performance reports, and managing projects. -->
+
+<?php
 
 ?>
